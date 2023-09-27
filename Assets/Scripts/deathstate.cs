961f04985833b1b2311a8b28ce53a9d934e6f0f8
HeinzDBV <heinzdubois@gmail.com>
2023-09-27 15:47:22 -0500

    Muerte dentro de objetos
    aaaaa

--- Assets/Scripts/deathstate.cs
+++ Assets/Scripts/deathstate.cs
@@ -1,13 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class deathstate : MonoBehaviour
 {
+    public bool isinDead;
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.CompareTag("Platforms") || other.gameObject.CompareTag("Enemy"))
+        {   
+            isinDead = true;
+        }
+        else
+        {
+            isinDead = false;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        isinDead = false;
     }
 
     // Update is called once per frame
