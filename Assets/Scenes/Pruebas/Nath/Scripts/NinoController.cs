e7e2d49b449a3897f1055549e940f3e115c5256f
nathkalantar <143037154+nathkalantar@users.noreply.github.com>
2023-10-14 12:42:45 -0500

    Ahora si en la main

--- Assets/Scenes/Pruebas/Nath/Scripts/NinoController.cs
+++ Assets/Scenes/Pruebas/Nath/Scripts/NinoController.cs
@@ -5,58 +5,47 @@ using UnityEngine;
 public class NinoController : MonoBehaviour
 {
     public float moveSpeed = 1.0f; // Velocidad de movimiento del NPC.
-    public Transform[] waypoints; // Los 4 puntos que el NPC visitará en orden.
+    public float moveDistance = 2.0f; // Distancia máxima que el NPC recorrerá en cada dirección.
     public Transform player;
     public float distancia;
 
-    private int currentWaypointIndex = 0; // Índice del punto actual.
-    private Animator animator; // Referencia al Animator del NPC.
+    private bool isWalking = false; // Indica si el NPC está caminando.
+    private Vector3 originalPosition; // Posición original del NPC.
+    private Vector3[] moveDirections = { Vector3.forward, Vector3.back, Vector3.left, Vector3.right }; // Direcciones de movimiento en el eje X y Z.
+    private int currentDirectionIndex = 0; // Índice de la dirección actual.
 
     void Start()
     {
-        animator = GetComponent<Animator>(); // Obtener la referencia al Animator del NPC.
-        transform.position = waypoints[0].position; // Coloca al NPC en el primer punto.
-
-        StartCoroutine(WalkToWaypoints());
+        originalPosition = transform.position;
+        StartCoroutine(WalkInDirections());
     }
 
     void Update()
     {
         distancia = (transform.position - player.position).magnitude;
-
-        if (Input.GetKeyDown(KeyCode.E) && distancia <= 3f)
+        if (Input.GetKeyDown(KeyCode.E) && distancia<=3f)
         {
             // El jugador presionó la tecla E, detiene al NPC.
-            StopCoroutine(WalkToWaypoints());
-
-            // Configura el parámetro "IsWalking" en el Animator como falso.
-            if (animator != null)
-            {
-                animator.SetBool("IsWalking", false);
-            }
+            StopCoroutine(WalkInDirections());
+            isWalking = false;
         }
     }
 
-    IEnumerator WalkToWaypoints()
+    IEnumerator WalkInDirections()
     {
-        // Configura el parámetro "IsWalking" en el Animator como verdadero.
-        if (animator != null)
+        isWalking = true;
+        while (isWalking)
         {
-            animator.SetBool("IsWalking", true);
-        }
-
-        while (true)
-        {
-            // Obtiene el siguiente punto de destino.
-            Transform currentWaypoint = waypoints[currentWaypointIndex];
+            // Calcula el destino actual en la dirección actual.
+            Vector3 currentDestination = originalPosition + moveDirections[currentDirectionIndex] * moveDistance;
 
             // Mueve al NPC hacia el destino actual.
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, currentDestination, moveSpeed * Time.deltaTime);
 
-            // Si llega al destino, avanza al siguiente punto.
-            if (Vector3.Distance(transform.position, currentWaypoint.position) < 0.01f)
+            // Si llega al destino, cambia a la siguiente dirección.
+            if (Vector3.Distance(transform.position, currentDestination) < 0.01f)
             {
-                currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+                currentDirectionIndex = (currentDirectionIndex + 1) % moveDirections.Length;
             }
 
             yield return null;
