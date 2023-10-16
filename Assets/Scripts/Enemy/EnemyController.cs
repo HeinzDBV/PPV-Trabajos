758a7db8a606f44cd47a2f73e4d63434e86bdaac
nathkalantar <143037154+nathkalantar@users.noreply.github.com>
2023-10-15 23:56:05 -0500

    inclui los scripts de los enemigos de clase al proyecto

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(CapsuleCollider))]
+public class EnemyController : MonoBehaviour
+{
+    #region States
+    public IdleState IdleState;
+    public FollowState FollowState;
+    private State currentState;
+    #endregion
+
+    #region Parameters
+    public Transform Player;
+    public float DistanceToFollow = 4f;
+    public float DistanceToAttack = 3f;
+    public float Speed = 1f;
+    public GameObject prefabStone;
+    public Transform FirePoint;
+    public float CoolDownTime = 1.0f;
+    #endregion
+
+    #region Readonly Properties
+    public Rigidbody rb { private set; get; }
+    public Animator animator { private set; get; }
+    #endregion
+
+
+
+    private void Awake()
+    {
+        IdleState = new IdleState(this);
+        FollowState = new FollowState(this);
+
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        // Seteamos el estado inicial
+        currentState = IdleState;
+    }
+
+    private void Start()
+    {
+        currentState.OnStart();
+    }
+
+    private void Update()
+    {
+        foreach (var transition in currentState.Transitions)
+        {
+            if (transition.IsValid())
+            {
+                // Ejecutar Transicion
+                currentState.OnFinish();
+                currentState = transition.GetNextState();
+                currentState.OnStart();
+                break;
+            }
+        }
+        currentState.OnUpdate();
+    }
+
+    public void Fire()
+    {
+        GameObject stone = Instantiate(prefabStone, FirePoint.position, Quaternion.identity);
+        stone.GetComponent<FireMovement>().Direction =
+            Player.position - transform.position;
+    }
+
+}
---
+++ Assets/Scripts/Enemy/FireMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody)), RequireComponent(typeof(SphereCollider))]
+public class FireMovement : MonoBehaviour
+{
+    public Vector3 Direction;
+
+    [SerializeField]
+    private float Speed = 5f;
+    [SerializeField]
+    private float timeToDestroy = 3f;
+
+    private Rigidbody rb;
+    private float timer = 0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        rb.velocity = Direction.normalized * Speed;
+
+        timer += Time.deltaTime;
+        if (timer >= timeToDestroy)
+        {
+            FireDestroy();
+        }
+    }
+
+    private void FireDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // Hay una colision
+        // TODO: Falta hacerle danho al jugador
+        Debug.Log("Se destruye");
+        FireDestroy();
+    }
+}
